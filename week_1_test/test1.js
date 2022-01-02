ca16ba878c2766fcc7fd9cd0f3496f54ccdbc462
Vemula John Babu <90505955+JohnVemula1998@users.noreply.github.com>
2022-01-02 18:27:25 +0530

    Advanced week1 test

---
+++ week_1_test/test1.js
@@ -0,0 +1,64 @@
+let img1 = document.getElementById("img1")
+img1.addEventListener('click',function(){
+    img1.classList.toggle('hai');
+});
+let img2 = document.getElementById("img2")
+img2.addEventListener('click',function(){
+    img2.classList.toggle('hai');
+});
+let img3 = document.getElementById("img3")
+img3.addEventListener('click',function(){
+    img3.classList.toggle('hai');
+});
+let img4 = document.getElementById("img4")
+img4.addEventListener('click',function(){
+    img4.classList.toggle('hai');
+});
+let img5 = document.getElementById("img5")
+img5.addEventListener('click',function(){
+    img5.classList.toggle('hai');
+});
+let img6 = document.getElementById("img6")
+img6.addEventListener('click',function(){
+    img6.classList.toggle('hai');
+});
+let img7 = document.getElementById("img7")
+img7.addEventListener('click',function(){
+    img7.classList.toggle('hai');
+});
+let img8 = document.getElementById("img8")
+img8.addEventListener('click',function(){
+    img8.classList.toggle('hai');
+});
+let img9 = document.getElementById("img9")
+img9.addEventListener('click',function(){
+    img9.classList.toggle('hai');
+});
+let img10 = document.getElementById("img10")
+img10.addEventListener('click',function(){
+    img10.classList.toggle('hai');
+});
+let img11 = document.getElementById("img11")
+img11.addEventListener('click',function(){
+    img11.classList.toggle('hai');
+});
+let img12 = document.getElementById("img12")
+img12.addEventListener('click',function(){
+    img12.classList.toggle('hai');
+});
+let img13 = document.getElementById("img13")
+img13.addEventListener('click',function(){
+    img13.classList.toggle('hai');
+});
+let img14 = document.getElementById("img14")
+img14.addEventListener('click',function(){
+    img14.classList.toggle('hai');
+});
+let img15 = document.getElementById("img15")
+img15.addEventListener('click',function(){
+    img15.classList.toggle('hai');
+});
+let img16 = document.getElementById("img16")
+img16.addEventListener('click',function(){
+    img16.classList.toggle('hai');
+});
